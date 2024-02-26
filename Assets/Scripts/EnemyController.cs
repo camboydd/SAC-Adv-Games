d668447fe9291c2a030e389b40b991dba9ec8fa4
camboydd <98326906+camboydd@users.noreply.github.com>
2024-02-26 13:57:53 -0500

    Merge branch 'main' of https://github.com/camboydd/SAC-Adv-Games

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    public NavMeshAgent enemy;
+    public Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        enemy.SetDestination(player.position);
     }
 }
